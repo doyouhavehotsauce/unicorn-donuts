a19f6752596fa4ad68767718ddf174f9a3923790
doyouhavehotsauce <56141712+doyouhavehotsauce@users.noreply.github.com>
2022-09-20 21:45:01 -0400

    fix bug on desktop navigation when user scrolls

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     const hamburgerToggle = document.getElementById('hamburger')
+    let loadCount = 0
     hamburgerToggle.addEventListener('click', () => {
         
             const getNav = document.getElementById('nav-menu')
@@ -19,15 +20,28 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // }, { passive: true });
     window.addEventListener('scroll', () => {
+        console.log(window.scrollY)
         let grabNav = document.querySelector('.desktop-logo')
-        console.log('scrolling' + window.scrollY)
-        if(window.scrollY >= 100){
+
+        // when page loads - load count = 0 and pageLoad class is in place
+        // once you scroll past 100 remove pageLoad class and add scrolldown class
+        // if you scroll back up and scrollY is less than 100 remove scroll down class and add scroll up
+
+        if(window.scrollY > 100 && loadCount == 0){
             grabNav.classList.add('navScrollDown')
-            grabNav.classList.remove('navScrollUp')
-        } else {  
+            grabNav.classList.remove('navScrollLoad')
+            loadCount += 1
+        } else if(window.scrollY < 100 && loadCount >= 1){
             grabNav.classList.add('navScrollUp')
-            grabNav.classList.remove('navScrollDown')
-        }
+            grabNav.classList.remove('navScrollDOwn')
+            grabNav.classList.remove('navScrollLoad')
+            loadCount += 1
+        } else if (window.scrollY > 100 && loadCount >= 1){
+                grabNav.classList.add('navScrollDown')
+                grabNav.classList.remove('navScrollUp')
+                grabNav.classList.remove('navScrollLoad')
+                loadCount += 1
+            };
     })
     
     
